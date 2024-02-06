478120137b71a48a643bea75545e6935b3d37b8e
Tsubasa-paru <tubasa_12345_67890@yahoo.co.jp>
2024-02-06 09:26:19 +0900

    init, get permission

---
+++ app/src/main/java/com/example/hc_app/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.hc_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hc_app.ui.theme.HC_appTheme
+
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.permission.HealthPermission
+import androidx.health.connect.client.PermissionController
+import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.records.HeartRateRecord
+import android.content.Intent
+import android.net.Uri
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HC_appTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+        checkHealthConnectAvailability()
+    }
+
+    private fun checkHealthConnectAvailability() {
+        val providerPackageName = "com.google.android.apps.healthdata" // Use the correct provider package name
+        val availabilityStatus = HealthConnectClient.sdkStatus(this, providerPackageName)
+        if (availabilityStatus == HealthConnectClient.SDK_UNAVAILABLE) {
+            return // early return as there is no viable integration
+        }
+        if (availabilityStatus == HealthConnectClient.SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED) {
+            // Optionally redirect to package installer to find a provider, for example:
+            val uriString = "market://details?id=$providerPackageName&url=healthconnect%3A%2F%2Fonboarding"
+            this.startActivity(
+                Intent(Intent.ACTION_VIEW).apply {
+                    setPackage("com.android.vending")
+                    data = Uri.parse(uriString)
+                    putExtra("overlay", true)
+                    putExtra("callerId", providerPackageName)
+                }
+            )
+            return
+        }
+        val healthConnectClient = HealthConnectClient.getOrCreate(this)
+        // Issue operations with healthConnectClient
+    }
+
+    // Create a set of permissions for required data types
+    val PERMISSIONS =
+        setOf(
+            HealthPermission.getReadPermission(HeartRateRecord::class),
+            HealthPermission.getWritePermission(HeartRateRecord::class),
+            HealthPermission.getReadPermission(StepsRecord::class),
+            HealthPermission.getWritePermission(StepsRecord::class)
+        )
+
+    // Create the permissions launcher
+    val requestPermissionActivityContract = PermissionController.createRequestPermissionResultContract()
+
+    val requestPermissions = registerForActivityResult(requestPermissionActivityContract) { granted ->
+        if (granted.containsAll(PERMISSIONS)) {
+            // Permissions successfully granted
+        } else {
+            // Lack of required permissions
+        }
+    }
+
+    suspend fun checkPermissionsAndRun(healthConnectClient: HealthConnectClient) {
+        val granted = healthConnectClient.permissionController.getGrantedPermissions()
+        if (granted.containsAll(PERMISSIONS)) {
+            // Permissions already granted; proceed with inserting or reading data
+        } else {
+            requestPermissions.launch(PERMISSIONS)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HC_appTheme {
+        Greeting("Android")
+    }
+}
